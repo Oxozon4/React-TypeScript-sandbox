dc313a971f80c0cbc4df0a9d26dc89ac47819ca1
Oxozon <oxozon.o4@gmail.com>
2022-10-09 18:43:19 +0200

    add classes example

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import GuestList from './state/GuestList';
 // import UserSearch from './state/UserSearch';
-import EventComponent from './events/EventComponent';
-
+// import EventComponent from './events/EventComponent';
+import ClassUserSearch from './classes/UserSearch';
+ 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 
 root.render(
   <React.StrictMode>
-    <EventComponent />
+    <ClassUserSearch users={[{name: 'Sarah', age: 20}, {name: 'Test', age: 20}]}/>
   </React.StrictMode>
 );
 
