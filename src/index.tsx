60cd41de9fa19cefcd8089b40d4de1ea0a694db2
Oxozon <oxozon.o4@gmail.com>
2022-10-09 18:27:58 +0200

    add events example

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 
 root.render(
   <React.StrictMode>
-    <UserSearch />
+    <EventComponent />
   </React.StrictMode>
 );
 
