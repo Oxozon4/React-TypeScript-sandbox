eb9b8a07315067d347405700d4c6327bdd110294
Oxozon <oxozon.o4@gmail.com>
2022-10-09 19:56:43 +0200

    add refs example

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 // import GuestList from './state/GuestList';
 // import UserSearch from './state/UserSearch';
 // import EventComponent from './events/EventComponent';
-import ClassUserSearch from './classes/UserSearch';
+// import ClassUserSearch from './classes/UserSearch';
+import UserSearch from './refs/UserSearch';
  
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 
 root.render(
   <React.StrictMode>
-    <ClassUserSearch users={[{name: 'Sarah', age: 20}, {name: 'Test', age: 20}]}/>
+    <UserSearch />
   </React.StrictMode>
 );
 
